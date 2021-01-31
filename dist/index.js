25b2390d625b3b272371a70f31ed7e42e3d57a21
Spoilerdo <martijn.dormans@gmail.com>
2021-01-31 18:26:41 +0100

    update dist

--- dist/index.js
+++ dist/index.js
@@ -12,26 +12,31 @@ const webrequest = __nccwpck_require__(4069);
 async function run () {
   try{
     const response = await webrequest('https://gitlab.com/users/martijn.dormans/calendar.json', 'GET');
-    const json = JSON.parse(response);
+
+    if(!response.data) {
+      core.setFailed('no gitlab metrics found');
+    }
+
+    const json = response.data;
     let newJson = {};
     for (const [key, value] of Object.entries(json)) {
-        const date = new Date(key);
-        // convert to milliseconds
-        let newKey = date.getTime() / 1000;
-        newJson[newKey] = value;
-      }
+      const date = new Date(key);
+      // convert to milliseconds
+      let newKey = date.getTime() / 1000;
+      newJson[newKey] = value;
+    }
 
-      // write the file to the repository
-      var tempFile = tmp.fileSync({
-        dir: process.env.RUNNER_TEMP,
-        prefix: "gitlab-calendar",
-        postfix: ".json",
-        keep: true,
-        discardDescriptor: true
-      });
+    // write the file to the repository
+    var tempFile = tmp.fileSync({
+      dir: process.env.RUNNER_TEMP,
+      prefix: "gitlab-calendar",
+      postfix: ".json",
+      keep: true,
+      discardDescriptor: true
+    });
 
-      fs.writeFileSync(tempFile.name, newJson);
-      core.setOutput("result", tempFile.name);
+    fs.writeFileSync(tempFile.name, newJson);
+    core.setOutput("result", tempFile.name);
   } catch (error) {
       core.setFailed(error.message);
   }
@@ -8721,7 +8726,8 @@ async function webrequest(url, method) {
         method
     };
     try {
-        return await axios(config);
+        const response = await axios(config);
+        return response;
     } catch (error) {
         console.error(error);
     }
